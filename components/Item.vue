18a60b7945686d8359a4363be312d7fd5c74a58b
mastinoz <persiste1@gmail.com>
2018-06-25 20:56:04 +0100

    tests

---
+++ components/Item.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="item">
+        <div class="siteTitle">{{siteTitle}}</div>
+        <div class="siteImage" :style="{backgroundImage: 'url(' + siteImage + ')'}"></div>
+        <div class="siteProgress">{{Progress}}</div>
+        <div class="siteDescrition">{{siteDescrition}}</div>
+        <div class="siteOptions">
+            <div class="editSite">Modify</div>
+            <div class="hideSite">Hide</div>
+            <div class="goToTheSite">Open in new tab</div>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+    
+    export default{
+        props: ['siteTitle', 'siteDescrition', 'Progress', 'siteImage', 'id']
+    }
+</script>
+
+<style scoped>
+.item{
+  background-color: white;
+
+        box-sizing: border-box;
+        width: 280px;
+        border: 1px solid #ccc;
+        box-shadow: 0 2px 2px #aaa;
+        margin: 2px;
+}
+div{
+    background-color: #ccc;
+    padding: 3px;
+}
+.siteTitle, .siteDescrition, .siteProgress, .siteImage, .siteOptions{
+    margin-bottom: 1px;
+    line-height: 2;
+}
+.editSite, .hideSite, .goToTheSite{
+    border: 1px dotted rgba(29, 29, 83, 0.493);
+}
+.siteDescrition{
+    height: 100px;
+}
+.siteImage{
+        background-position: center;
+        background-size: cover;
+        width: 100%;
+        height: 200px;
+}
+.siteOptions{
+    width: 100%;
+  display: flex;
+  flex: align-self;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,101 @@
 <template>
   <section class="container">
-    <div>
+    <script src="https://cdn.jsdelivr.net/gh/GrabzIt/grabzit@js-3.2.9.4/javascript/grabzit.min.js"></script>
+    <header>
       <app-logo/>
-      <h1 class="title">
-        p_status
-      </h1>
-      <h2 class="subtitle">
+      <h1 class="utitle">
+        <h2>p_status</h2>
+      <h4 class="subtitle">
         Interface Status for all projects
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
+      </h4>
+      </h1>
+    </header>
+  <div class="main">
+    <item 
+    v-for="item in items" :key="item.id"
+    :siteImage="item.siteImage" 
+    :siteTitle="item.siteTitle"
+      :id="item.id" 
+      :siteDescrition="item.siteDescrition"
+       />
+
+  </div>
   </section>
 </template>
 
 <script>
 import AppLogo from '~/components/AppLogo.vue'
 
+import Item from '@/components/Item'
+
 export default {
   components: {
-    AppLogo
+    AppLogo,
+    Item
+  },
+
+        props: ['siteTitle', 'siteDescrition', 'siteImage'],
+
+  asyncData(){
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          items: [
+            {
+              id: "1",
+              siteTitle: "Google",
+              siteDescrition: "Google Google Google",
+              siteImage: "http://127.0.0.1:8887/google.png"
+            },
+            {
+              id: "2",
+              siteTitle: "Twitter",
+              siteDescrition: "Twitter Twitter Twitter",
+              siteImage: "http://127.0.0.1:8887/twitter.png"
+            },
+            {
+              id: "3",
+              siteTitle: "Github",
+              siteDescrition: "Github Github Github",
+              siteImage: "http://127.0.0.1:8887/github.png"
+            }
+          ]
+        })
+      }, 1500)
+    })
   }
-}
+};
+GrabzIt("NmI3ZGU4ZGI5NTEzNGFjZWE5Y2JjMWNjMzZkOGY4Y2U=").ConvertURL("http://www.google.com", 
+{"bwidth":1024,"bheight":768,"width":195,"height":146}).Create();
 </script>
 
 <style>
-.container {
+.container{
+  margin:0;
+  padding: 10px;
+  border: 3px solid gray;
   min-height: 100vh;
+}
+header {
+  /*min-height: 100vh;*/
   display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  justify-content: left;
+  align-items: left;
+  text-align: left;
 }
 
 .title {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
+  display: absolute;
   font-weight: 300;
-  font-size: 100px;
+  /* font-size: 100px; */
   color: #35495e;
   letter-spacing: 1px;
 }
 
 .subtitle {
   font-weight: 300;
-  font-size: 42px;
+  /* font-size: 42px; */
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
@@ -61,4 +104,27 @@ export default {
 .links {
   padding-top: 15px;
 }
+.main{
+  margin: 10px;
+  background-color: #35495e;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+    .recipe {
+        box-sizing: border-box;
+        width: 280px;
+        padding: 8px;
+        border: 1px solid #ccc;
+        box-shadow: 0 2px 2px #aaa;
+    }
+    
+    .thumbnail {
+        background-position: center;
+        background-size: cover;
+        width: 100%;
+        height: 200px;
+    }
 </style>
